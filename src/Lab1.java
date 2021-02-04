c810407786ed657de05a989b2a78e5d235212495
danielrosenthal2 <rosedb19@wfu.edu>
2021-02-04 12:28:38 -0500

    commit1

--- src/Lab1.java
+++ src/Lab1.java
@@ -1,5 +1,49 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Lab1 {
     public static void main(String[] args) {
-        System.out.println("Fix me!");
+
+        ArrayList<String> store_item = new ArrayList<String>();
+        store_item.add("Beans, $2");
+        store_item.add("Pineapple, $3");
+        store_item.add("Water, $1");
+        store_item.add("Apple Watch, $300");
+        store_item.add("Toothpaste, $0.50");
+
+
+        ArrayList<Double> store_price = new ArrayList<Double>();
+        store_price.add(2.0);
+        store_price.add(3.0);
+        store_price.add(1.0);
+        store_price.add(300.0);
+        store_price.add(0.5);
+
+
+        double subtotal = 0;
+        System.out.println("Store inventory and prices:");
+        for (int i = 0; i < store_item.size(); i++){
+            System.out.print(i + 1 + " ");
+            System.out.println(store_item.get(i));
+        }
+        boolean exit = false;
+        while (exit = false) {
+            System.out.println("Select number of item to add to cart:");
+            Scanner scnr = new Scanner(System.in);
+            int item = 0;
+            try {
+                item = scnr.nextInt() - 1;
+                System.out.println(store_item.get(item));
+                subtotal = subtotal + store_price.get(item);
+                System.out.println(subtotal);
+                System.out.println("Do you want to continue shopping? Enter yes or no.");
+                if (scnr.nextLine() == "No"); {
+                    exit = true;
+                }
+
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("Exception thrown: " + e);
+            }
+        }
     }
 }

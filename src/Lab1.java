0dfa0a981b6b46be4a1a8055de9652d803b5aec3
danielrosenthal2 <rosedb19@wfu.edu>
2021-02-08 16:11:09 -0500

    commit5

--- src/Lab1.java
+++ src/Lab1.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
 public class Lab1 {
     public static void main(String[] args) {
 
-        ArrayList<String> store_item = new ArrayList<String>();
+        //private class store {
+            //Names of items in store
+            ArrayList<String> store_item = new ArrayList<String>();
         store_item.add("Beans, $2");
         store_item.add("Pineapple, $3");
         store_item.add("Water, $1");
@@ -12,30 +14,36 @@ public class Lab1 {
         store_item.add("Toothpaste, $0.50");
         store_item.add("Chips, $3.50");
 
-
-        ArrayList<Double> store_price = new ArrayList<Double>();
+            //Prices of items
+            ArrayList<Double> store_price = new ArrayList<Double>();
         store_price.add(2.0);
         store_price.add(3.0);
         store_price.add(1.0);
         store_price.add(300.0);
         store_price.add(0.5);
         store_price.add(3.5);
-
-
+        //}
+        //Initializing variables
         double subtotal = 0, sales_tax = 0, total = 0;
+
+        //Presenting list of items in store w/ prices
         System.out.println("Store inventory and prices:");
         for (int i = 0; i < store_item.size(); i++){
             System.out.print(i + 1 + " ");
             System.out.println(store_item.get(i));
         }
+
+        //Initializing more variables
         boolean exit = false;
         int item = 0;
         String yes;
 
-        while (!exit) {
+        //While loop to continuously ask for inout from command line
+        //while (!exit) {
             System.out.println("Select number of item to add to cart:");
             Scanner scnr = new Scanner(System.in);
 
+            //try and catch for out of bounds exception
             try {
                 item = scnr.nextInt() - 1;
                 System.out.println(store_item.get(item));
@@ -44,17 +52,17 @@ public class Lab1 {
             }
             subtotal = subtotal + store_price.get(item);
             sales_tax = subtotal*0.05;
-            total = subtotal*sales_tax;
+            total = subtotal + sales_tax;
             System.out.println("Subtotal: $" + subtotal);
             System.out.println("Sales tax: $" + sales_tax);
             System.out.println("Total: $" + total);
-            System.out.println("Do you want to continue shopping? Enter yes or no.");
-            yes = scnr.nextLine();
-            if (scnr.nextLine() == "yes") {
-                exit = false;
-            } else {
-                exit = true;
-            }
-        }
+            //System.out.println("Do you want to continue shopping? Enter yes or no.");
+            //yes = scnr.nextLine();
+            //if (scnr.nextLine() == "yes") {
+               // exit = false;
+            //} else if (scnr.nextLine() == "no") {
+                //exit = true;
+            //}
+        //}
     }
 }
